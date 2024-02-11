946192d8f8ab1d8e4b4aca60c8334b04e3d535cb
tarakokko3233 <tanahashist1230@outlook.jp>
2024-02-12 01:02:31 +0900

    一旦リセット

--- eemon551/app/src/main/java/com/example/eemon551/MainActivity.java
+++ eemon551/app/src/main/java/com/example/eemon551/MainActivity.java
@@ -1,25 +1,19 @@
 package com.example.eemon551;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.database.Observable;
+import androidx.room.Room;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
-
-        textView = findViewById(R.id.text_question_name);
-    }}
+        setContentView(R.layout.activity_main);
+    }
+}
