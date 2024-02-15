4f9c64bfa03a08e1bafd8c657c6c31b48c92b498
tarakokko3233 <tanahashist1230@outlook.jp>
2024-02-15 13:09:04 +0900

    url変更3

--- eemon_551/eemon_551/urls.py
+++ eemon_551/eemon_551/urls.py
@@ -14,5 +14,5 @@ router.register(r'userquestiondatas', views.UserQuestionDataViewSet)
 # APIのURLパターンをurlpatternsに追加
 urlpatterns = [
     path('', include(router.urls)),
-    path('userquestiondatas', UserQuestionDataDelete.as_view()),
+    path('delete_userquestiondata/', views.UserQuestionDataDelete.as_view()),
 ]
