17ae1483ddfc1924dc59bc8380b3d4099fe635c6
zono0013 <143684883+zono0013@users.noreply.github.com>
2024-02-15 18:02:58 +0900

    androidappの画面遷移

--- eemon551/app/src/main/java/com/example/eemon551/MainActivity.java
+++ eemon551/app/src/main/java/com/example/eemon551/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Intentを作成してGameActivityを起動
-                Intent intent = new Intent(MainActivity.this, game.class);
+                Intent intent = new Intent(MainActivity.this, activity_home.class);
                 startActivity(intent);
             }
         });
