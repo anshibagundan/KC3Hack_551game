9ce5148ebe424da43a24e2b1cca559cc28604892
tarakokko3233 <tanahashist1230@outlook.jp>
2024-02-16 12:24:50 +0900

    dbモデル変更

--- eemon_551/eemon_551_app/models.py
+++ eemon_551/eemon_551_app/models.py
@@ -1,32 +1,42 @@
 from django.db import models
 
-# Locationモデル
 class Location(models.Model):
-    name = models.CharField(max_length=255)
-    font = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False)
     img = models.TextField(default='no image')
-    iskansai = models.BooleanField()
+    iskansai = models.BooleanField(default=False)
 
-# Genreモデル
+    def __str__(self):
+        return self.name
+
 class Genre(models.Model):
-    name = models.CharField(max_length=255)
-    col = models.CharField(max_length=6, default='000000')
+    name = models.CharField(max_length=255, null=False)
 
-# Questionモデル
+    def __str__(self):
+        return self.name
+
 class Question(models.Model):
-    name = models.CharField(max_length=255)
-    img = models.TextField(default='no image')
-    txt = models.TextField()
+    name = models.CharField(max_length=255, null=False)
+    img = models.BinaryField(blank=True, null=True)  # DjangoのBinaryFieldを使用
+    txt = models.TextField(null=False)
     link = models.CharField(max_length=255, blank=True, null=True)
-    loc_id = models.ForeignKey(Location, on_delete=models.CASCADE)
-    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    loc_id = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='questions')
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='questions')
 
-# UserDataモデル
+    def __str__(self):
+        return self.name
+
 class UserData(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False)
+    score = models.IntegerField(default=0)
+    combo = models.IntegerField(default=0)
 
-# UserQuestionDataモデル
+    def __str__(self):
+        return self.name
+
 class UserQuestionData(models.Model):
-    user_data_id = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    user_data_id = models.ForeignKey(UserData, on_delete=models.CASCADE, related_name='question_data')
     cor = models.BooleanField()
-    qes_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    qes_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='user_data')
+
+    def __str__(self):
+        return f"{self.user_data_id.name} - {self.qes_id.name}"
