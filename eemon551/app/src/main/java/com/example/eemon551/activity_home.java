3b6d94184731c3868b293939651f39479511a817
zono0013 <143684883+zono0013@users.noreply.github.com>
2024-02-16 14:00:04 +0900

    genre名の取り出し

--- eemon551/app/src/main/java/com/example/eemon551/activity_home.java
+++ eemon551/app/src/main/java/com/example/eemon551/activity_home.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class activity_home extends AppCompatActivity {
 
@@ -15,6 +23,7 @@ public class activity_home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         Button startButton = findViewById(R.id.start);
+        TextView text_genre= findViewById(R.id.genre);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +31,31 @@ public class activity_home extends AppCompatActivity {
                 // Intentを作成してGameActivityを起動
                 Intent intent = new Intent(activity_home.this, game.class);
                 startActivity(intent);
+            }
+        });
+
+        ApiService apiService = ApiClient.getApiService();
+
+        // APIリクエストを実行
+        apiService.getAllQuestions().enqueue(new Callback<List<Question>>() {
+            @Override
+            public void onResponse(Call<List<Question>> call, Response<List<Question>> response) {
+                if (response.isSuccessful() && response.body() != null && !response.body().isEmpty()) {
+                    // 最初の質問のnameを取得してTextViewにセット
+                    int genre_id = response.body().get(0).getGenre_id();
+                    String genre;
+                    if(genre_id == 1){
+                        genre = "食べ物";
+                    }else{
+                        genre = "全て";
+                    }
+                    text_genre.setText(genre);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Question>> call, Throwable t) {
+                // エラーハンドリング
+                Log.e("MainActivity", "APIリクエスト失敗: ", t);
             }
         });
     }
