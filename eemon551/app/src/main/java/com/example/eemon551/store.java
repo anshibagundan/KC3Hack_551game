248eac1a904e7fe41d419c7a10496249f0be5243
tarakokko3233 <tanahashist1230@outlook.jp>
2024-02-19 13:29:12 +0900

    写真かーどたこ焼き

--- eemon551/app/src/main/java/com/example/eemon551/store.java
+++ eemon551/app/src/main/java/com/example/eemon551/store.java
@@ -3,12 +3,17 @@ package com.example.eemon551;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class store extends AppCompatActivity {
 
+    private TextView money;
+    private ApiService apiService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+
     }
 }
