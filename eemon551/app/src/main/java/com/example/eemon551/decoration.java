8d00d848d42d0675a4ce4981fab16c64417ff7da
keisukessu <149043402+keisukessu@users.noreply.github.com>
2024-02-21 23:59:42 +0900

    デコレーションのホーム下

--- eemon551/app/src/main/java/com/example/eemon551/decoration.java
+++ eemon551/app/src/main/java/com/example/eemon551/decoration.java
@@ -2,7 +2,9 @@ package com.example.eemon551;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class decoration extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class decoration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decoration);
+    }
+
+    public void decoration_home(View view){
+        Intent intent = new Intent(decoration.this, activity_home.class);
+        startActivity(intent);
+    }
+    public void decoration_zukann(View view){
+        Intent intent = new Intent(decoration.this, garally.class);
+        startActivity(intent);
+    }
+    public void decoration_store(View view){
+        Intent intent = new Intent(decoration.this, store.class);
+        startActivity(intent);
+    }
+    public void decoration_introduce(View view){
+        Intent intent = new Intent(decoration.this, introduce.class);
+        startActivity(intent);
     }
 }
