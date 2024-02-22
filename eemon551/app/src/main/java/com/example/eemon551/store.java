e1be97c97f5fa8d5bbf15e031ee2f450dbf26046
zono0013 <143684883+zono0013@users.noreply.github.com>
2024-02-22 21:27:02 +0900

    無理すぎる

--- eemon551/app/src/main/java/com/example/eemon551/store.java
+++ eemon551/app/src/main/java/com/example/eemon551/store.java
@@ -14,10 +14,12 @@ import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -33,10 +35,12 @@ public class store extends AppCompatActivity {
     private ApiService apiService;
     private GridLayout gridLayout_1;
     private Set<Integer> displayedQuestionIds = new HashSet<>();
+    private List<Question> all_QuestionList = new ArrayList<>();
     private FrameLayout store_screen;
     private FrameLayout card_screen;
     private FrameLayout title_screen;
     private FrameLayout back_screen;
+    private GridLayout card_layout;
     private TextView back_cost,back_cost2;
     private ImageView buy_back,buy_back2;
     private TextView title,title2,title3,title_cost,title_cost2,title_cost3,buy_title,buy_title_cost;
@@ -45,6 +49,14 @@ public class store extends AppCompatActivity {
     private int collect_money;
 
     private int userId;
+    private Integer randomValue;
+    private int card_link;
+    private ImageView card_1;
+    private ImageView card_2;
+    private ImageView card_3;
+    private ImageView card_4;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,7 +70,7 @@ public class store extends AppCompatActivity {
         store_screen = findViewById(R.id.store);
         //カード関連
         card_screen = findViewById(R.id.buy_card_screen);
-
+        card_layout = findViewById(R.id.card_layout);
         card_ano = findViewById(R.id.card_ano);
 
         //background関連
@@ -79,13 +91,43 @@ public class store extends AppCompatActivity {
         buy_title = findViewById(R.id.buy_title);
         buy_title_cost = findViewById(R.id.buy_title_cost);
         title_ano = findViewById(R.id.title_ano);
+        card_1 = findViewById(R.id.card_1);
+        card_2 = findViewById(R.id.card_2);
+        card_3 = findViewById(R.id.card_3);
+        card_4 = findViewById(R.id.card_4);
 
 
 
         GetMoney();
-//        for (int i = 0; i < 6; i++) {
-//            fetchQuestions();
-//        }
+        fetchQuestions();
+    }
+    private void fetchQuestions() {
+        apiService.getAllQuestions().enqueue(new Callback<List<Question>>() {
+            @Override
+            public void onResponse(Call<List<Question>> call, Response<List<Question>> response) {
+                if (response.isSuccessful() && response.body() != null && !response.body().isEmpty()) {
+                    all_QuestionList.clear();
+                    Log.d("API Request Failure", "all_be");
+                    for (Question question : response.body()) {
+                        Log.d("API Request Failure", "all_in");
+                        all_QuestionList.add(question);
+                    }
+                    Log.d("API Request Failure", "all_af");
+                    for (Question question : all_QuestionList) {
+                        Log.d("API Request Failure", "false_in" + ":question.getId()="+ question.getId() +" :userId=" + userId);
+                        applyOverlayBasedOnUserQuestionData(question.getId(), 10);
+                    }
+                    Log.d("API Request Failure", "false_af");
+                    Log.d("API Request Failure", "card_deco_in_be");
+                    card_deco();
+                }
+
+            }
+            @Override
+            public void onFailure(Call<List<Question>> call, Throwable t) {
+                Log.e("API Request Failure", "Error: ", t);
+            }
+        });
     }
 
     private void GetMoney(){
@@ -106,48 +148,81 @@ public class store extends AppCompatActivity {
         });
     }
 
+    private void applyOverlayBasedOnUserQuestionData(int questionId, int userId) {
+        Log.d("API Request Failure", "false_tuika001");
 
-//    private void fetchQuestions() {
-//
-//            apiService.getAllQuestions().enqueue(new Callback<List<Question>>() {
-//                @Override
-//                public void onResponse(Call<List<Question>> call, Response<List<Question>> response) {
-//
-//                        Random random = new Random();
-//                        int questionNo = random.nextInt(10);
-//                    if (response.isSuccessful() && response.body() != null && !response.body().isEmpty()) {
-//                        Question question = response.body().get(questionNo);
-//
-//                            if (!displayedQuestionIds.contains(question.getId()) && question.getLoc_id() >= 1 && question.getLoc_id() <= 6) {
-//                                DisplayQuestion(question);
-//                                displayedQuestionIds.add(question.getId());
-//                            } else {
-//                                //もし出題するidが出題済みidリストに存在したらもう一回ロード
-//                                fetchQuestions();
-//                            }
-//                        }
-//
-//                }
-//
-//
-//                @Override
-//                public void onFailure(Call<List<Question>> call, Throwable t) {
-//                    Log.e("API Request Failure", "Error: ", t);
-//                }
-//            });
-//        }
-//    private void DisplayQuestion(Question question) {
-//
-//            ImageView imageView = new ImageView(this);
-//            String img = question.getCard().replace("\"", "").trim();
-//            int imageResId = getResources().getIdentifier(img, "drawable", getPackageName());
-//            Glide.with(this)
-//                    .load(imageResId)
-//                    .override(600, 400) // 画像の解像度を指定
-//                    .into(imageView);
-//            gridLayout_1.addView(imageView);
-//
-//    }
+        //----ここから下が動作してない。。。。。もう無理
+        apiService.getUserQuestionData(userId).enqueue(new Callback<List<UserQuestionData>>() {
+            @Override
+            public void onResponse(Call<List<UserQuestionData>> call, Response<List<UserQuestionData>> response) {
+                Log.d("API Request Failure", "false_tuika00");
+                try
+                {if (response.isSuccessful() && response.body() != null) {
+                    List<UserQuestionData> userQuestionData = response.body();
+                    Log.d("API Request Failure", "false_tuika0");
+                    for (UserQuestionData data : userQuestionData) {
+                        Log.d("API Request Failure", "false_tuika1");
+                        if (data.get_qes_id() == questionId && data.getUser_data_id() == userId) {
+                            Log.d("API Request Failure", "false_tuika2");
+                            if (!data.get_cor()) {
+                                displayedQuestionIds.add(questionId);
+                                Log.d("API Request Failure", "false_tuika3");
+                            }
+                        }
+                    }
+                }} catch (Exception e) {
+                Log.e("API Request Failure", "Error in onResponse", e);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<UserQuestionData>> call, Throwable t) {
+                Log.e("API Request Failure", "Error: ", t);
+            }
+        });
+    }
+
+    private int Res_cardId(){
+        Log.d("API Request Failure", "random_be");
+        randomValue = getRandomElement(displayedQuestionIds);
+        Log.d("API Request Failure", "random_af");
+        apiService.getQuestionById(randomValue).enqueue(new Callback<Question>() {
+            @Override
+            public void onResponse(Call<Question> call, Response<Question> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    String img = response.body().getCard().replace("\"", "").trim();
+                    card_link = getResources().getIdentifier(img, "drawable", getPackageName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Question> call, Throwable t) {
+                Log.e("API Request Failure", "Error: ", t);
+            }
+        });
+        Log.d("API Request Failure", "all_af" + card_link);
+        return card_link;
+    }
+    private <T> T getRandomElement(Set<T> set) {
+        List<T> list = new ArrayList<>(set);
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
+    }
+    private void card_deco(){
+        Log.d("API Request Failure", "card_deco_in");
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) {
+                Log.d("API Request Failure", "card_deco1");
+                card_1.setImageResource(Res_cardId());
+            } else if (i == 1) {
+                card_2.setImageResource(Res_cardId());
+            } else if (i == 2) {
+                card_3.setImageResource(Res_cardId());
+            } else if (i == 3) {
+                card_4.setImageResource(Res_cardId());
+            }
+        }
+    }
 
     public void buy_card(View view) {
         //int cost = Integer.parseInt(buy_card.getText().toString());
