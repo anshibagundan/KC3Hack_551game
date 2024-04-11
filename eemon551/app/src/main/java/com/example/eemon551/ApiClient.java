e629c4e9ed56295cf9fa3da60e9f04931bdce3b8
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-04-11 10:40:35 +0900

    Update ApiClient.java

--- eemon551/app/src/main/java/com/example/eemon551/ApiClient.java
+++ eemon551/app/src/main/java/com/example/eemon551/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "https://eemon-551.onrender.com/";
+    private static final String BASE_URL = "https://eemon-04247001a29b.herokuapp.com";
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService() {
